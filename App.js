cc817500af26aff741d531c64059af0117329483
chayanin <chayanin2007@gmail.com>
2017-04-02 00:01:56 +0630

    minimum templete

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ScrollView, Image, Text, View } from 'react-native';
+import * as Progress from './components/react-native-progress';
 
-export default class App extends React.Component {
+export default class ProcessActivity extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View>
+        <View style={styles.TopView}>
+          <Text style={styles.TopText}>This is subject</Text>
+        </View>
+        <View style={styles.MidView}>
+          <Text style={styles.MidText}> หมวดหมู่: </Text>
+          <Text style={styles.MidText}> สถานที่เกิดเหตุ: </Text>
+          <Text style={styles.MidText}> เวลาเกิดเหตุ: </Text>
+        </View>
+        <Image source={require('./img/pic1.jpg')} />
+        <Text>เจ้าหน้าที่รับเรื่องแล้ว</Text>
+        <Progress.Bar progress={0.5} width={270} />
+        <View style={styles.button}>
+          <Text style={color= 'white'}>ยกเลิก</Text>
+        </View>
       </View>
     );
   }
@@ -19,5 +31,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  MidView: {
+    margin: 2,
+    backgroundColor: '#fff',
+  },
+  MidText: {
+    color: 'black',
+    fontWeight: 'bold',
+  },
+  TopView: {
+    backgroundColor: 'rgba(0, 122, 255, 1)',
+    alignItems: 'center',
+    padding: 10,
+  },
+  TopText: {
+    color: 'white',
+    fontSize: 30,
+  },
+  button: {
+    backgroundColor: 'brown',
+    padding: 7,
   },
 });
