c2d1533614dd773716505aeba01540a6304d55e5
Phiyawat Vongsawat <phiyawat9597@gmail.com>
2017-04-02 06:41:38 +0700

    Design Request

--- components/Home.js
+++ components/Home.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View ,Button} from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 class Home extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-          <View style={styles.viewouter}>
-            <Button style={styles.button} onPress={()=>this.props.navigation.navigate('Report')} title="แจ้งความ"></Button>
-            <View style={styles.viewinner}/>
-            <Button style={styles.button} onPress={()=>this.props.navigation.navigate('Follow')} title="ติดตามผล"></Button>
-          </View>
-          <View style={styles.logout}>
-            <Button title="ออกจากระบบ"></Button>
-          </View>  
+        <View style={styles.viewouter}>
+          <Button
+            style={styles.button}
+            onPress={() => this.props.navigation.navigate("Report")}
+            title="แจ้งความ"
+          />
+          <View style={styles.viewinner} />
+          <Button
+            style={styles.button}
+            onPress={() => this.props.navigation.navigate("Follow")}
+            title="ติดตามผล"
+          />
+        </View>
+        <View style={styles.logout}>
+          <Button title="ออกจากระบบ" />
+        </View>
       </View>
     );
   }
@@ -21,26 +29,26 @@ class Home extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
-  botton:{
-      width:200,
-      height:50,
-      backgroundColor:'#5bc0de',
-      color:'white'
+  botton: {
+    width: 200,
+    height: 50,
+    backgroundColor: "#5bc0de",
+    color: "white"
   },
-  viewinner:{
-      width:50
+  viewinner: {
+    width: 50
   },
-  logout:{
-      width:120
+  logout: {
+    width: 120
   },
-  viewouter:{
-      flexDirection:'row',
-      height:70,
-      marginBottom:10
+  viewouter: {
+    flexDirection: "row",
+    height: 70,
+    marginBottom: 10
   }
 });
 
