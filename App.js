83b81250e834eb4c8798e2ba898180a8fe3bb8fc
chayanin <chayanin2007@gmail.com>
2017-04-02 01:41:46 +0630

    first page end

--- App.js
+++ App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import { StyleSheet, ScrollView, Image, Text, View } from 'react-native';
+import { StyleSheet, ScrollView, Image, Text, View, TouchableOpacity } from 'react-native';
 import * as Progress from './components/react-native-progress';
+import button from './components/button/button';
 
 export default class ProcessActivity extends React.Component {
   render() {
     return (
       <View>
         <View style={styles.TopView}>
-          <Text style={styles.TopText}>This is subject</Text>
+          <Text style={styles.TopText}>รายละเอียดแจ้งเหตุ</Text>
         </View>
         <View style={styles.MidView}>
-          <Text style={styles.MidText}> หมวดหมู่: </Text>
-          <Text style={styles.MidText}> สถานที่เกิดเหตุ: </Text>
-          <Text style={styles.MidText}> เวลาเกิดเหตุ: </Text>
-        </View>
-        <Image source={require('./img/pic1.jpg')} />
-        <Text>เจ้าหน้าที่รับเรื่องแล้ว</Text>
-        <Progress.Bar progress={0.5} width={270} />
-        <View style={styles.button}>
-          <Text style={color= 'white'}>ยกเลิก</Text>
+          <Text><Text style={styles.MidText}>หมวดหมู่: </Text><Text>จราจร</Text></Text>
+          <Text><Text style={styles.MidText}>สถานที่เกิดเหตุ: </Text><Text>ซอย สาธรแมนชั่น 1 แขวง คลองต้นไทร เขต คลองสาน กรุงเทพมหานคร 10600</Text></Text>
+          <Text><Text style={styles.MidText}>เวลาเกิดเหตุ: </Text><Text>10:15    2/4/60</Text></Text>
+          <View style={styles.PicView}>
+            <Image source={require('./img/pic1.jpg')} style={{width: 200, height: 200}} />
+          </View>
+          <View style={styles.ProgbarView}>
+            <Progress.Bar progress={0.6666666} width={270} />
+            <Text>เจ้าหน้าที่รับเรื่องแล้ว</Text>
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <View style={{flex:1}}></View>
+            <View style={{flex:2}}>
+            <TouchableOpacity style={styles.ButtonContainer}>
+              <Text style={styles.ButtonText}>ตกลง</Text>
+            </TouchableOpacity>
+            </View>
+            <View style={{flex:1}}></View>
+            <View style={{flex:2}}>
+            <TouchableOpacity style={styles.ButtonContainer}>
+              <Text style={styles.ButtonText}>ยกเลิก</Text>
+            </TouchableOpacity>
+            </View>
+            <View style={{flex:1}}></View>
+          </View>
         </View>
       </View>
     );
@@ -33,24 +50,43 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   MidView: {
-    margin: 2,
     backgroundColor: '#fff',
+    marginLeft: 10,
+    marginRight: 10,
   },
   MidText: {
     color: 'black',
     fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  PicView: {
+    alignItems: 'center',
+    marginBottom: 25,
+    marginTop: 25,
+  },
+  ProgbarView: {
+    marginBottom: 25,
+    alignItems: 'center',
   },
   TopView: {
     backgroundColor: 'rgba(0, 122, 255, 1)',
     alignItems: 'center',
     padding: 10,
+    marginBottom: 25,
   },
   TopText: {
     color: 'white',
     fontSize: 30,
   },
-  button: {
-    backgroundColor: 'brown',
-    padding: 7,
+  ButtonContainer: {
+    backgroundColor: 'rgba(0, 122, 255, 1)',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    alignItems: 'center',
+  },
+  ButtonText: {
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: '700',
   },
 });
